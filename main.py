2b6679b09037289a3bf1d39b5b16ab1075782081
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-15 05:53:47 +0000

    Improve the conn format

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ my_email = os.environ.get("GMAIL")
 password = os.environ.get("GMAIL_PASS")
 receiver_email = os.environ.get("YMAIL")
 
-conn = smtplib.SMTP("smtp.gmail.com")
-conn.starttls()
-conn.login(user=my_email, password=password)
-conn.sendmail(from_addr=my_email,
-              to_addrs=receiver_email,
-              msg="Subject:Testing\n\nTesting python smtp connections")
-conn.close()
+with smtplib.SMTP("smtp.gmail.com") as conn:
+    conn.starttls()
+    conn.login(user=my_email, password=password)
+    conn.sendmail(from_addr=my_email,
+                  to_addrs=receiver_email,
+                  msg="Subject:Hello\n\nBody of the email")
