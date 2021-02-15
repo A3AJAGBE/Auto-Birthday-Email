12026ae1e94d389738cf2526903953c0c6975041
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-15 06:26:44 +0000

    Send weekly motivation email

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
 import smtplib
 import os
+import datetime as dt
+import random
 from dotenv import load_dotenv
 load_dotenv()
 
+my_email = os.environ.get("GMAIL")
+password = os.environ.get("GMAIL_PASS")
+receiver_email = os.environ.get("YMAIL")
 
-my_email = os.environ.get("YMAIL")
-password = os.environ.get("YMAIL_pass")
-receiver_email = os.environ.get("GMAIL")
+# Get the current day
+current = dt.datetime.now()
+day_of_the_week = current.weekday()
 
-with smtplib.SMTP("smtp.mail.yahoo.com") as conn:
-    conn.starttls()
-    conn.login(user=my_email, password=password)
-    conn.sendmail(from_addr=my_email,
-                  to_addrs=receiver_email,
-                  msg="Subject:Hello G\n\nBody oby of the awesome type.")
+# Be sure it's a monday
+if day_of_the_week == 0:
+    # Get a random quote
+    with open("quotes.txt") as quote_data:
+        quotes = quote_data.readlines()
+        quote = random.choice(quotes)
+
+    # Forward the email containing the quote
+    with smtplib.SMTP("smtp.gmail.com") as conn:
+        conn.starttls()
+        conn.login(user=my_email, password=password)
+        conn.sendmail(from_addr=my_email,
+                      to_addrs=receiver_email,
+                      msg=f"Subject:Weekly Motivation\n\nThis week's quote is {quote}")
