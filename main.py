13f82c891fc87703c815798d0a76cedfa1f1ee50
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-15 07:51:35 +0000

    Get person info and replace name in template

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import datetime as dt
 import pandas as pd
+import random
 
 # Get the current day and month
 current = dt.datetime.now()
@@ -11,5 +12,21 @@ today = (day, month)
 data = pd.read_csv("birthday_information.csv")
 data_dict = {(row['day'], row['month']): row for (index, row) in data.iterrows()}
 
+PLACEHOLDER = "[NAME]"
+
+# Check if someone's birthday is today
+if today in data_dict:
+    # Get the person
+    person = data_dict[today]
+    # Select a random letter template
+    num = random.randint(1, 3)
+    letter_template = f"letterTemplates/letter{num}.txt"
+
+    # Open the letter
+    with open(letter_template) as template:
+        content = template.read()
+        # Replace the placeholder with the person's name
+        letter = content.replace(PLACEHOLDER, person['name'])
+        print(letter)
 
 
