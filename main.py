5555fb2fd281661f484294a9ba5dfafda519b7ec
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-02-15 07:40:05 +0000

    Get day and month in birthday information

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import datetime as dt
+import pandas as pd
 
-# Get the current day
+# Get the current day and month
 current = dt.datetime.now()
 day = current.day
 month = current.month
-year = current.year
-today = (day, month, year)
-print(today)
+today = (day, month)
 
+# The day and month in the birthday information
+data = pd.read_csv("birthday_information.csv")
+data_dict = {(row['day'], row['month']): row for (index, row) in data.iterrows()}
 
 
 
